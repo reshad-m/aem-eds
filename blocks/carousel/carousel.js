4e595cf0c495ea111c099cdeb45f3e0cccc6810a
Reshad Mirazai <reshad.mirazai@thepixel.com>
2025-01-23 15:21:32 +0000

    row

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -138,9 +138,7 @@ export default async function decorate(block) {
       indicator.innerHTML = `<button type="button" aria-label="${placeholders.showSlide || 'Show Slide'} ${idx + 1} ${placeholders.of || 'of'} ${rows.length}"></button>`;
       slideIndicators.append(indicator);
     }
-
-    // Append the row back to the block
-    block.append(row);
+    // row.remove();
   });
 
   container.append(slidesWrapper);
