416fdd99ba4786c8e8cc7edd7982625436aa4c91
Reshad Mirazai <reshad.mirazai@thepixel.com>
2025-04-14 23:34:12 +0100

    Refactor testimonials block: streamline DOM manipulation and improve structure

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -1,29 +1,30 @@
 import decorateSlider from '../../scripts/common/carousel.js';
 import { loadCSS } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   try {
-    const container = document.createElement('div');
+    const newWrapper = document.createElement('div');
+    
     [...block.children].forEach((row) => {
-      const testimonial = document.createElement('div');
-      const content = document.createElement('div');
-      moveInstrumentation(row, testimonial);
+      const newRow = document.createElement('div');
+      const contentDiv = document.createElement('div');
       [...row.children].forEach((div) => {
-        content.appendChild(div.firstElementChild);
-        moveInstrumentation(div, content);
-
+        const children = [...div.children];
+        children.forEach(child => {
+          contentDiv.append(child);
+        });
       });
-      testimonial.appendChild(content);
-      container.appendChild(testimonial);
+      
+      newRow.append(contentDiv);
+      newWrapper.append(newRow);
     });
-    console.log('Block:', block);
-    console.log('Container:', container);
+    
     block.textContent = '';
-    block.append(...container.children);
+    block.append(...newWrapper.children);
     await loadCSS(`${window.hlx.codeBasePath}/styles/common/carousel.css`);
     await decorateSlider(block);
   } catch (error) {
     // eslint-disable-next-line no-console
+    console.log('Failed to initialize testimonials carousel', error);
   }
 }
