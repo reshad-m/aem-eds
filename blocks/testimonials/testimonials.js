3e78900a87febc86ccb299aacbf89836c25db57c
Reshad Mirazai <reshad.mirazai@thepixel.com>
2025-04-14 23:34:55 +0100

    Refactor testimonials block: improve code readability by standardizing arrow function syntax and removing unnecessary whitespace

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -4,21 +4,21 @@ import { loadCSS } from '../../scripts/aem.js';
 export default async function decorate(block) {
   try {
     const newWrapper = document.createElement('div');
-    
+
     [...block.children].forEach((row) => {
       const newRow = document.createElement('div');
       const contentDiv = document.createElement('div');
       [...row.children].forEach((div) => {
         const children = [...div.children];
-        children.forEach(child => {
+        children.forEach((child) => {
           contentDiv.append(child);
         });
       });
-      
+
       newRow.append(contentDiv);
       newWrapper.append(newRow);
     });
-    
+
     block.textContent = '';
     block.append(...newWrapper.children);
     await loadCSS(`${window.hlx.codeBasePath}/styles/common/carousel.css`);
