c091eec8a94d84d065313bccff7472ada8e959b3
Reshad Mirazai <reshad.mirazai@thepixel.com>
2025-04-14 23:26:37 +0100

    Refactor testimonials block: restructure decoration logic and enhance error handling

--- blocks/testimonials/testimonials.js
+++ blocks/testimonials/testimonials.js
@@ -1,5 +1,27 @@
 import decorateSlider from '../../scripts/common/carousel.js';
+import { loadCSS } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  await decorateSlider(block);
+  try {
+    const container = document.createElement('div');
+    [...block.children].forEach((row) => {
+      const testimonial = document.createElement('div');
+      const content = document.createElement('div');
+      moveInstrumentation(row, testimonial);
+      [...row.children].forEach((div) => {
+        content.appendChild(div.firstElementChild);
+      });
+      testimonial.appendChild(content);
+      container.appendChild(testimonial);
+    });
+
+    block.textContent = '';
+    block.append(...container.children);
+    await loadCSS(`${window.hlx.codeBasePath}/styles/common/carousel.css`);
+    await decorateSlider(block);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('Failed to initialize testimonials carousel');
+  }
 }
